4efc5947102bcf354da94c773bc144f0ecca1d96
Jacob <73615055+jacobdev03@users.noreply.github.com>
2024-01-28 18:29:53 +0100

    .\src\components\Cards.css
    postion cards in grid

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,11 +1,11 @@
 import './Card.css';
 
-const Card = ({ data, handleClick }) => {
+const Card = ({ data, handleClick, id }) => {
   if (!data) {
     return <p>Fetching.....</p>;
   }
   return (
-    <div className="card" onClick={handleClick}>
+    <div className="card" id={id} onClick={handleClick}>
       <img src={data.images.original.url} alt={data.title} />
     </div>
   );
