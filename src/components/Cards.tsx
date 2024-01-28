1516c50f6053ce2fc99bb4e399ff88c89ea304b6
Jacob <73615055+jacobdev03@users.noreply.github.com>
2024-01-28 18:29:05 +0100

    tracking score functionality

--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -1,15 +1,22 @@
 import Card from './Card';
-import { useEffect, useState } from 'react';
+import { memo, useEffect, useState } from 'react';
 import './Cards.css';
 
 const Cards = () => {
   const [data, setData] = useState<string[]>([]);
+  const [score, setScore] = useState<number>(0);
+  const [bestScore, setBestScore] = useState<number>(0);
+  const [memorizedCards, setMemorizedCards] = useState<string[]>([]);
+
+  if (score > bestScore) {
+    setBestScore(score);
+  }
 
   useEffect(() => {
     fetch(
       `https://api.giphy.com/v1/gifs/search?api_key=${
         import.meta.env.VITE_API_KEY
-      }&q=cat&limit=10&offset=0&rating=g&lang=en&bundle=messaging_non_clips`,
+      }&q=cat&limit=12&offset=0&rating=g&lang=en&bundle=messaging_non_clips`,
       {
         method: 'GET',
         headers: {},
@@ -28,8 +35,22 @@ const Cards = () => {
     return array;
   };
 
-  const handleClick = () => {
+  const checkMemorizedCards = (id) => {
+    if (memorizedCards.includes(id)) {
+      console.log('You lost');
+      setScore(0);
+      setMemorizedCards([]);
+    } else {
+      setScore(score + 1);
+      setMemorizedCards([id, ...memorizedCards]);
+    }
+  };
+
+  const handleClick = (e) => {
+    console.log(e.currentTarget);
     const arrayToShuffle = [...data];
+    checkMemorizedCards(e.currentTarget.id);
+
     const shuffledArray = shuffle(arrayToShuffle);
     setData(shuffledArray);
   };
@@ -37,11 +58,19 @@ const Cards = () => {
   if (!data) return <p>loading</p>;
 
   return (
-    <div className="cards-container">
-      {data.map((cardData) => {
-        return <Card key={cardData.id} data={cardData} handleClick={handleClick} />;
-      })}
-    </div>
+    <>
+      <div className="sub-heading">
+        Score: <h2>{score}</h2> <br />
+        Best Score: <h2>{bestScore}</h2>
+      </div>
+      <div className="cards-container">
+        {data.map((cardData) => {
+          return (
+            <Card id={cardData.id} key={cardData.id} data={cardData} handleClick={handleClick} />
+          );
+        })}
+      </div>
+    </>
   );
 };
 
